e5e277efac4a29c8e7880676cacda69340578930
Nurevsan Ozturan <nurevsangorduu@gmail.com>
2023-06-02 10:44:55 +0600

    02.06

--- src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
+++ src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
@@ -1,19 +1,20 @@
 package base_urls;
-
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
-import io.restassured.specification.*;
+import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
-
 public class JsonPlaceHolderBaseUrl {
-    //setUp() methodu ile request işleminde tekrarlı şekilde yapılacak işlemler
-    // burada bir kez yapılacak. @Test öncesi çalışması için @Before anatasyonu ekliyoruz.
+    //setUp() methodu ile request işleminde tekrarlı şekilde yapılacak işlemler burada bir kez yapılacak. @Test öncesi çalışması için @Before anatasyonu ekliyoruz.
+
     protected RequestSpecification spec;
 
-    @Before
-    public void setUp(){
-        spec = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com/").setAccept(ContentType.JSON).build();
-
+    @Before//Her @Test methodu öncesei çalışır
+    public void setUp() {
+        spec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri("https://jsonplaceholder.typicode.com/")
+                .setAccept(ContentType.JSON)
+                .build();
     }
 }
--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -5,13 +5,28 @@ import java.util.Map;
 
 public class JsonPlaceHolderTestData {
 
-    public Map<String,Object> expectedDataMapMethod(int userId,String title,boolean completed){
-        Map<String,Object> expectedData = new HashMap<>();
-        expectedData.put("userId",userId);
-        expectedData.put("title",title);
-        expectedData.put("completed",completed );
+    public Map<String, Object> expectedDataMapMethod(Integer userId, String title, Boolean completed) {
 
+        Map<String, Object> expectedData = new HashMap<>();
+        if(userId != null){
+            expectedData.put("userId", userId);
+        }
+        if(title!=null){
+            expectedData.put("title", title);
+        }
+        if (completed != null){
+            expectedData.put("completed", completed);
+        }
         return expectedData;
+    }
+
+    public static String expectedDataInString(int userId, String title, boolean completed){
+
+        return "{\n" +
+                "\"userId\": "+userId+",\n" +
+                "\"title\": \""+title+"\",\n" +
+                "\"completed\": "+completed+"\n" +
+                "}";
 
     }
 }
